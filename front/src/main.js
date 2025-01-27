01d2d498fff4334a0fa2e070fb09462f93cc2435
Iaas22 <ishaaamin22@gmail.com>
2025-01-27 08:48:52 +0100

    merge cambios

--- front/src/main.js
+++ front/src/main.js
@@ -1,8 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';  // Importamos el router
-import $ from 'jquery';
-import 'https://code.jquery.com/jquery-3.4.1.slim.min.js';
+
 
 
 // Crear la instancia de Vue y usar Vue Router
